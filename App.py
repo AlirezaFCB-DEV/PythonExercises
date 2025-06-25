437ca3e19ce97062bc63a6e47ecfa7903dd8d2be
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-25 14:10:36 +0330

    end section1

--- App.py
+++ App.py
@@ -5,6 +5,53 @@ print("#js IS Best Ever In Languages too PytHon Love")
 num1 = 12
 num2 = 13
 price = num1 + num2
-print(price) #! 25 
-print(price)
+print(price)  # ! 25
 
+print(5j + 3)
+
+print(r"js dev ali \n js")
+print("js dev ali \n js")
+print("""Hello Iam Alireza
+      A FullStack Developer And Ai Developer And ...
+      
+      Hello This Is A test
+      """)
+print("""Hello Iam Alireza
+      A FullStack Developer And Ai Developer And ...
+      
+      Hello This Is A test
+      """)
+
+print("Python" + "Is A Best Programming Lang:)")
+print("Python" "Is A Best Programming Lang:)")
+print("Python" * 3)
+
+str = "jssssssss"
+
+print(str.__len__())  # Show Length
+
+print(str[0])
+print(str[1])
+print(str[3])
+print(str[-3])
+print(str[-2])
+print(str[-0])
+
+str2 = "Hello Im Alireza A Best Boxer And Champion On The World!!"
+
+print(str2[0: 5])
+
+print("line44 " + str2[5:])
+print("line45 " + str2[:5])
+print("line46" + str2[:-5])
+print("line47" + str2[-2:])
+print(str[4:] + str[:4])
+print(str2[2:] + str2[:2])
+
+str3 = """+---+---+---+---+---+---+
+ | P | y | t | h | o | n |
+ +---+---+---+---+---+---+
+ 0   1   2   3   4   5   6
+-6  -5  -4  -3  -2  -1"""
+
+print(str3)
