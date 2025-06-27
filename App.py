fdbdba662d5ccb34dd5a997b6a972ab5e4c88c2b
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-27 13:14:04 +0330

    4.5. else Clauses on Loops

--- App.py
+++ App.py
@@ -1,12 +1,7 @@
-# for n in range(2, 10):
-#     for x in range(2, n):
-#         # print(f"line 4 {x} {n}")
-#         if n % x == 0:
-#             print(f"line 6 {n} equals {x} * {n // x}")
-#             break
-
-for number in range(2, 20):
-    if number % 2 == 0 :
-        print(f"Found An Even Number : {number}")
-        continue
-    print(f"Found An Odd Number : {number}")
+for n in range(1, 10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(f"{n} equals {x} * {n // x}")
+            break
+    else:
+        print(f"{n} is a prime number")
