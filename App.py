a92d14a90c1d7308856059753f8e6c4678460029
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-27 12:49:25 +0330

    4.4. break and continue Statements

--- App.py
+++ App.py
@@ -1,27 +1,12 @@
-for i in range(10):
-    print(i + 1)
+# for n in range(2, 10):
+#     for x in range(2, n):
+#         # print(f"line 4 {x} {n}")
+#         if n % x == 0:
+#             print(f"line 6 {n} equals {x} * {n // x}")
+#             break
 
-for i in range(5, 10):
-    print(i)
-
-for i in range(0, 10, 3):
-    print(i)
-
-for i in range(-1, -100, -10):
-    print(i)
-
-alireza = "alireza"
-
-for w in alireza:
-    print(w)
-
-words = ["alireza", 15, "js", "react", 18, ["js", "angular", "react"]]
-
-for i in range(len(words)):
-    print(i, words[i])
-
-print(range(10))
-
-print("line 25: ", sum(range(10)))
-# print("line 26: ", sum(8)) #! error for that number is not iterable
-
+for number in range(2, 20):
+    if number % 2 == 0 :
+        print(f"Found An Even Number : {number}")
+        continue
+    print(f"Found An Odd Number : {number}")
