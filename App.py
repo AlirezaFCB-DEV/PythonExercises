e6bcd8b02671f5a5383ef1d51ec26b55782373d2
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-06-29 11:27:40 +0330

    4.9.2 Keyword Arguments

--- App.py
+++ App.py
@@ -1,25 +1,4 @@
-def say_hello (name) :
-    print(f"Hello {name}")
-    
-say_hello("Alireza")
+def func1(name, age):
+    print(f"Hello {name} With  {age} YearsOld")
 
-def sum (num1  , num2) :
-    return num1 + num2
 
-print(sum(51 , 73))
-
-def user_info (name , age = 16 , city="Tehran"):
-    print(name , age , city )
-    
-user_info("Alireza" , city="Karaj")
-
-def greet (name = "Ghost") :
-    print(name)
-
-# greet()
-greet("Alireza")
-
-def x (*args) :
-    print(args)
-
-x(15 , "alireza" , True  , ["js" , "js"] , {"id" : 12})  
