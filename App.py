34ad2e7adef0566e5108df21480aed3fd0769181
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-01 10:13:40 +0330

    5.1.1 – Using Lists as Stacks

--- App.py
+++ App.py
@@ -1,16 +1,11 @@
-def func (name : str) -> None :
-    print(name)
-    
-func("Alireza")
+stack = []
 
-def add (num1: int , num2 : int) -> int :
-    """
-    This Func Get Two Parameters That First Num1 That Force Number And Second That Force Number 
-    This Func Return A Number Of Plus Num1 To Num2
-    """
-    return num1 + num2
+stack.append(1)
+stack.append(2)
+stack.append(3)
+stack.append(4)
+stack.append(5)
 
-add(20 , 25)
-
-
-        
+print(stack)
+print(stack.pop())
+print(stack)
