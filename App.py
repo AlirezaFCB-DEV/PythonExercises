b5b9ddfe150d076cda49552320d4f3fd7beaa9ff
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-01 10:11:18 +0330

    4.9.8 – Function Annotations

--- App.py
+++ App.py
@@ -1,15 +1,16 @@
-def func () : 
-    """this function says Hello World!"""
-    print("Hello World!")
+def func (name : str) -> None :
+    print(name)
     
-    
-func()
+func("Alireza")
 
-def add (num1 , num2) :
+def add (num1: int , num2 : int) -> int :
     """
-    this func plus num1 to num2 and return response
+    This Func Get Two Parameters That First Num1 That Force Number And Second That Force Number 
+    This Func Return A Number Of Plus Num1 To Num2
     """
     return num1 + num2
 
+add(20 , 25)
 
-print(add(1 , 5))
+
+        
