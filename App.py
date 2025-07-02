9fdce20816f5ea7199bdbade7e407682805a3d2c
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-02 11:13:31 +0330

    5.1.2 – Using Lists as Queues

--- App.py
+++ App.py
@@ -1,11 +1,17 @@
-stack = []
+from collections import deque
 
-stack.append(1)
-stack.append(2)
-stack.append(3)
-stack.append(4)
-stack.append(5)
+q = deque([1 , 2 ,3 ])
+q.append(4)
+print(q.popleft())
+print(q)
 
-print(stack)
-print(stack.pop())
-print(stack)
+#! Top A Good Code
+
+queue = [1, 2, 3]
+queue.append(4)
+queue.append(5)
+
+print(queue.pop(0)) 
+print(queue)
+
+#! Top A bad code , For Bad Performance In Big Lists
