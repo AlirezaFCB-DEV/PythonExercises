d81057a252a715cac93b82b73acc0ed2448cac32
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-03 12:34:18 +0330

    5.6. Looping Techniques

--- App.py
+++ App.py
@@ -1,6 +1,19 @@
-user = {"name" : "Alireza" , "age" : 16} 
-print(user["name"])
+users = ["Alireza", "Komeyl", "mojtaba", "Marzie"]
 
-print(user.keys()) 
-print(user.values()) 
-print(user.items()) 
+for index, value in enumerate(users):
+    print(index, value)
+
+
+names = ["Ali", "Sara"]
+ages = [16, 18]
+
+for name, age in zip(names, ages):
+    print(f"{name} {age} years old")
+
+for item in sorted(set("Banana")):
+    print(item)
+
+users2 = {"Alireza", "mojtaba", "zxa", "nh", "marzie", "komeyl"}
+
+for item in sorted(users2):
+    print(item)
