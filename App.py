4f2bfe1293559ab66c2405d1c425cbe86bdd7072
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-03 12:11:17 +0330

    5.4. Sets

--- App.py
+++ App.py
@@ -1,3 +1,18 @@
-tuple_list  = (1 , 2 , 3 ,4)
-# del tuple_list[1] #! Error This Code For Tuples Is Immutable
-print(tuple_list)
+basket = {"LapTop" , "Mog" , "KeyBoard" , "Boxing Glows" , "Boxing Band"}
+
+print(type(basket))
+print(basket)
+
+print("Js" in basket)
+print("Boxing Glows" in basket)
+
+#################
+
+a = set("jst")
+b = set("react")
+
+print(a - b) #! Items that are in a but not in b
+# print(a | b)
+print(b | a) #! their community
+print(a & b) #! Shared Item
+print(a ^ b) #! Just In One Sets
