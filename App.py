6b2f37dd78397fbe445dc75868bdd71f647da25f
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-03 12:40:08 +0330

    5.7. More on Conditions

--- App.py
+++ App.py
@@ -1,19 +1,9 @@
-users = ["Alireza", "Komeyl", "mojtaba", "Marzie"]
+name = input("Please Inter YourName: ")
+age = int(input("Please Enter YourAge : "))
 
-for index, value in enumerate(users):
-    print(index, value)
-
-
-names = ["Ali", "Sara"]
-ages = [16, 18]
-
-for name, age in zip(names, ages):
-    print(f"{name} {age} years old")
-
-for item in sorted(set("Banana")):
-    print(item)
-
-users2 = {"Alireza", "mojtaba", "zxa", "nh", "marzie", "komeyl"}
-
-for item in sorted(users2):
-    print(item)
+if name == "Alireza" and age == 17 :
+    print("Is Owner")
+elif name not in {"alireza" , "dev" ,  "js"} or age not in {16 , 17 , 18} : 
+    print("Is User")
+    
+    
