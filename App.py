8f7c689707b34239c6a140270f277b1fa1594aa6
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-03 12:20:49 +0330

    5.5. Dictionaries

--- App.py
+++ App.py
@@ -1,18 +1,6 @@
-basket = {"LapTop" , "Mog" , "KeyBoard" , "Boxing Glows" , "Boxing Band"}
+user = {"name" : "Alireza" , "age" : 16} 
+print(user["name"])
 
-print(type(basket))
-print(basket)
-
-print("Js" in basket)
-print("Boxing Glows" in basket)
-
-#################
-
-a = set("jst")
-b = set("react")
-
-print(a - b) #! Items that are in a but not in b
-# print(a | b)
-print(b | a) #! their community
-print(a & b) #! Shared Item
-print(a ^ b) #! Just In One Sets
+print(user.keys()) 
+print(user.values()) 
+print(user.items()) 
