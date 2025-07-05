4c33da4d80bc35722c9f8032b601f445eb8a7c6e
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-05 11:28:56 +0330

    6.4.1. Importing * From a Package

--- App.py
+++ App.py
@@ -1,7 +1,12 @@
 import random
+from packages import Module1 
+from packages import Module2
 
 print(dir(random))
 print("----")
 print(dir([]))
 print("----")
 print(dir(""))
+
+Module1.hello()
+Module2.by()
