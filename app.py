5b5a58e371aa6a205155530cfb4116e520ed2f1f
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-16 15:50:13 +0330

    8.1. Syntax Errors

--- app.py
+++ app.py
@@ -1,15 +1,3 @@
-import json
-
-data = {
-    "name" : "komeyl",
-    "age" : 9
-}
-
-with open("user.json" , "w") as f :
-    json.dump(data, f)
-    
-with open("user.json" , "r") as f:
-    user = json.load(f)
-    
-    
-print(user)
+# if True 
+#     print("HI")
+#! The Syntax Error   
