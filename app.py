cac5aacac80cb1b23c3f629de2e0876b0d8b307a
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-16 15:56:31 +0330

    8.3. Handling Exceptions

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-print(10 / 0) #! ZeroDivisionError
-int("ABC") #! Value Error
+try :
+    num1 = int(input("Please Enter A Number :"))
+    print(100 / num1)
+except ValueError :
+    print("Please Enter Number !!")
+except ZeroDivisionError :
+    print("Don't Divide By Zero!")
+    
+try :
+    num2 = int(input("Please Enter A Number : "))
+    print(num2 * 5)
+except Exception as e :
+    print(f"Error: {e}")
