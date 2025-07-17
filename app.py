5ad8c74ca2a2d4d4e4d55936474d36e6dadfca34
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-17 14:07:11 +0330

    8.10. Enriching Exceptions with Notes

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
-# try : 
-#     print("salam")
-# except ValueError :
-#     print("value is invalid")
-# except TypeError :
-#     print("type is invalid")
-
-############
-
 try :
-    print("code ...")
-except (ValueError , TypeError) as e :
-    print(f"Handled Error : {e}")
-    
+    raise ValueError("invalid input")
+except ValueError as e :
+    e.add_note("please make sure the input is numeric")
+    raise
