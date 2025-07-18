f50185ceed5a237724c2c7198c49d7e58025a19e
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-18 09:05:24 +0330

    9.3.1. Class Definition Syntax

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
-class MyClass:
-    pass
+class car:
+    def __init__(self, name: str, model: str, year: int, color: str):
+        self.name = name
+        self.model = model
+        self.year = year
+        self.color = color
+
+    def run(self, speed: int) -> str:
+        print(f"This Car Can Driving With {speed}KM/H")
 
 
-obj = MyClass()
+BugattiChiron = car("Bugatti", "Chiron", 2028, "WhiteBlue")
+print(BugattiChiron)
+BugattiChiron.run(450)
