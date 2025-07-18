b8eca125db4cbb2c4d0181e62f51aa1093d9de4d
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-18 08:27:50 +0330

    9.2. Python Scopes and Namespaces

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-a = [1 , 2 , 3]
-b = a 
-b.append(4)
-print(a) #* [1, 2, 3, 4]
+def outer():
+    x = "enclosing"
+
+    def inner():
+        x = "local"
+        print(x)  # ! local scope
+
+    inner()
+    print(x)  # ! enclosing scope
+
+
+x = "global"
+
+outer()
+
+print(x)  # *global
+
+# print() #len() #... #! Built-in scope
