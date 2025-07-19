c647f671f4e7f4ef34a716e1c7ba62d9c1ef637b
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-19 11:33:33 +0330

    9.3.2. Class Objects

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
-class car:
-    def __init__(self, name: str, model: str, year: int, color: str):
-        self.name = name
-        self.model = model
-        self.year = year
-        self.color = color
-
-    def run(self, speed: int) -> str:
-        print(f"This Car Can Driving With {speed}KM/H")
-
-
-BugattiChiron = car("Bugatti", "Chiron", 2028, "WhiteBlue")
-print(BugattiChiron)
-BugattiChiron.run(450)
+class MyClass :
+  js = "js"
+  
+print(MyClass)
+print(MyClass.js)
