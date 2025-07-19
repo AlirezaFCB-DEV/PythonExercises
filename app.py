a87376be7f908ad18c8ceddb2908f509143cea03
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-19 11:40:57 +0330

    9.3.3. Instance Objects

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
-class MyClass :
-  js = "js"
-  
-print(MyClass)
-print(MyClass.js)
+class Person:
+    x = 12
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+a = Person("Alireza", 16)
+b = Person("Mojtaba", 47)
+
+a.x = "jsjsjs"
+
+b.x = "TsTsTsTs"
+
+print(a)
+print(b)
+print(a.x)
+print(b.x)
