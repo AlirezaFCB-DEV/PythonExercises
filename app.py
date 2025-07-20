633c3754d2cbb4680d21072ae836640b2fd1304b
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-20 10:22:55 +0330

    9.8. Iterators

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
-class Car :
-    def __str__(self):
-        return "This Is A Car"
-        
-BugattiChiron = Car()
+list1 = [1, 2, 3]
 
-print(BugattiChiron)
-print(isinstance(BugattiChiron , Car))
+it = iter(list1)
+
+print(next(it))
+print(next(it))
+
+
+class Nums:
+    def __iter__(self):
+        self.a = 1
+        return self
+
+    def __next__(self):
+        x = self.a
+        self.a += 1
+        return x
+
+
+num2 = Nums()
+
