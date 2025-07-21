0c12297817a6602d60b97bbfa88314162ac2be4f
AlirezaDev00 <alirezadevloperui@gmail.com>
2025-07-21 10:48:18 +0330

    10.11. Quality Control

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-import timeit
+import doctest
 
-print(timeit.timeit('"-".join(str(n) for n in range(100))' , number=10000))
+def square(x):
+    """
+    >>> square(2)
+    4
+    """
+    
+    return x * x
+
+doctest.testmod()
